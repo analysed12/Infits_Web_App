1c15eab137c17a7eeb68408340c0cb03dd9735da
User <shamanthgm3@gmail.com>
2022-12-30 11:57:43 +0530

    added the my_plans.php page

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'infits');
+define('DB_NAME', 'infits1');
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 //Check the connection
