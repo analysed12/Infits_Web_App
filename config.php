393c6ec808eaeed51f1dbce7217f1e923bc04f6e
Anim3sh <76643531+Anim3shSinha@users.noreply.github.com>
2023-01-07 19:23:25 +0530

    added past template

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME', 'infits1');
+define('DB_NAME', 'infits');
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 //Check the connection
