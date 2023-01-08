bd92c04310ef9b106042aed6d3aab0972b56d6e7
priyanka0926 <pr.priyanka28@gmail.com>
2023-01-08 21:59:42 +0530

    add -ingredients.php

--- Add-ingredients.php
+++ Add-ingredients.php
@@ -1,156 +1,369 @@
-<?php include ('connection.php'); ?>
-    
-<?php
-//$name = $_SESSION['receipe_name'];
-//$id = $_SESSION['ingrd_id'];
-if(isset($_POST['Done']) || isset($_POST['add-more'])  && isset($_FILES['my_image']))   
-{        
-        
-       $ingrd1 =$_POST['text1'];
-
-       $quantity1 =$_POST['text2'];
-
-       //image upload 
-           $img_name= $_FILES['my_image']['name'];
-             $img_size = $_FILES['my_image']['size'];
-             $tmp_name = $_FILES['my_image']['tmp_name'];
-               $error =$_FILES['my_image']['error'];
-                $file_type= $_FILES['my_image']['type'];
-              if($error === 0)
-              {
-                 if($img_size > 209712)
-                 {
-                       echo("file too large");
-
-                  }
-                  else
-                 {
-                          $img_ex=pathinfo($img_name, PATHINFO_EXTENSION);
-                         $img_ex_lc=strtolower($img_ex);
-                        $allowed_ex = array("jpg","png");
-
-                          if(in_array($img_ex_lc, $allowed_ex))
-                         {
-                            $new_name = uniqid("IMG-", true).'.'.$img_ex_lc;
-                           $img_upload_path = "./images/" .$new_name;
-              
-                           move_uploaded_file($tmp_name,$img_upload_path );  
-                           $imageandpath="$new_name|$img_upload_path";
-                           //$final = $imageandpath | $ingrd1 | $quantity1;
-           
-              
-       $sql = "update `add_ingredient` set `image` = '$imageandpath', `ingrd_name` = '$ingrd1',`quantity` =  '$quantity1' 
-       where `ingrd_id` = 620";
-;
-    if($conn-> query($sql)  == TRUE)
-    {
-      echo "instruction added";
-            
-    }
-        else
-        {
-          echo "data not Inserted";
-    }
-  }
-} } } ?>
-
-
-  
 
 
 <!Doctype html>
 <html>
     <title> Add Ingredients </title>
     <head>
-      <link rel="stylesheet" href="./css/add_ingrd.css">
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infits</title>
+  
+    <script type="text/JavaScript" 
+src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js">
+</script>
+
+      
 
      <!-------code for image upload in plus sign------>
      <script type="text/JavaScript" 
 src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js">
 </script>
 
+<style>
+    #content {
+   overflow: auto;
+   display: flex;
+   flex-direction: column;
+   height: 90%;
+   font-family: 'Poppins';
+   font-style: normal;
+   padding: 10px;
+   margin: 0px;
+}
+
+/*  image   */
+.custom-file-input {
+color: transparent;
+}
+.custom-file-input::-webkit-file-upload-button {
+visibility: hidden;
+}
+.custom-file-input::before {
+content: 'Upload Photo';
+color: black;
+display: inline-block;
+background: -webkit-linear-gradient(top, #f9f9f9, #e3e3e3);
+border: none;
+border-radius: 3px;
+padding: 30px;
+outline: none;
+white-space: nowrap;
+-webkit-user-select: none;
+cursor: pointer;
+text-shadow: 1px 1px #fff;
+font-weight: 700;
+font-size: 10pt;
+}
+.custom-file-input:hover::before {
+border-color: black;
+}
+.custom-file-input:active {
+outline: 0;
+}
+.custom-file-input:active::before {
+background: -webkit-linear-gradient(top, #e3e3e3, #f9f9f9); 
+}
+
+/*   tabs      */
+/* Style the tab */
+.tab {
+display: flex;
+justify-content: space-around;
+background-color: #FAFAFA;
+border-radius: 10px;
+}
+
+/* Style the buttons inside the tab */
+.tab button {
+background-color: inherit;
+border-radius: 10px;
+float: left;
+border: none;
+padding: 10px 40px;
+outline: none;
+cursor: pointer;
+transition: 0.3s;
+font-size: 17px;
+margin: 5px;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.activee {
+background-color: #D257E6;
+color: white;
+}
+
+/* Style the tab content */
+.tabcontent {
+display: none;
+padding: 6px 30px;
+border: none;
+border-top: none;
+color: black;
+left:456px;
+
+}
+.flex-box{
+  display:flex;
+  flex-direction:row;
+  margin-left:40%;
+
+}
+.box{
+  height:55px;
+  width:55px;
+  border-radius:5px;
+  border-color: #7963FB;
+  border: 1px solid;
+  text-align:center;
+  display:flex;
+
+}
+.link{
+  display:flex;
+  justify-content:space-between;
+  margin-left:20px;
+}
+#link-click{
+  color:black;
+  font-size:20px;
+  margin-top:15px;
+  margin-left:10px;
+  cursor:pointer;
+}
+<!-------popup style----->
+.add-ingrd{
+  height: 320px;
+  width: 380px;
+  border-radius:15px;
+  display:none;
+  top:40%;
+  left:50%;
+  color:black;
+  border-style: 5px solid #BF59EA;
+  font-size:20px;
+  font-weight:400;
+
+}
+.flex-box1{
+  display:flex;
+  flex-direction:column;
+  flex-wrap:wrap;
+}
+ .ingrd-header{
+  display:flex;
+  justify-content:space-around;
+  height:50px;
+  margin-left:20px;
+  margin-top:15px;
+
+ }
+.ingrd-content{
+
+  display:flex;
+  flex-direction:row;
+  justify-content:space-between;
+  margin-top:50px;
+  margin-left:5px;
+
+
+}
+.image-add{
+  display:flex;
+  text-align:center;
+  height:56px;
+  width:56px;
+  border-radius:5px;
+  color:#BF59EA;
+  border:1px solid;
+  margin-top:40px;
+}
+.left-flex{
+  display:flex;
+  flex-direction:column;
+}
+
+.input1{
+  width:250px;
+  height:45px;
+  border-radius:8px;
+  margin-left:20px;
+}
+#add{
+  height:48px;
+  width:178px;
+  border-radius:15px;
+  background-color:#BF59EA;
+  color:white;
+  cursor:pointer;
+  margin-left:20px;
+}
+  </style>
+
+
+
+  <!------js for add ingredients popup---->
+  <script>
+              $(documet).ready(function(){
+                $("#open-ingd").click(function(){
+                  $("#add-ingrd").modal('show');
+
+                });
+
+              });
+
+
+            </script>
+
+
       
   
          <script>
+ 
+          //$(function(){
+           // $('#img_add').on('click',function(){
+           //   $('#my_image').trigger('click');
 
-          $(function(){
-            $('#sign').on('click',function(){
-              $('#my_image').trigger('click');
+           $(document).ready(function(){
+            $("#img_add").click(funcion(){
+            $("#my_image").trigger('click');
+          
             });
           });     
     
         </script>
 </head>
 <body>
-             <div class="container">
-           <!------sideNav---------->
-          <?php include ('event_calendar.php');?>
+     
+  <!------connection ------>
+  <?php include('connection.php'); ?>
+  <?php
+         //$ingid= $_SESSION['id'];
+         $ingid = 841;
+      if(isset($_POST['add']))
+      {
+        $ingrd = $_POST['ingrd-name'];
+        $quanty =$_POST['quantity'];
+  
+    
+                      $sql ="update `add_ingredient` set  `ingrd_name` = '$ingrd',
+                       `quantity` = '$quanty' where `ingrd_id` = $ingid";
 
-         /* <?php
-            //    $sql ="select * from `dietian_recipies` where `dietitianuserID`='Sam'";
-             //   $res = mysqli_query($conn,$sql);
-            //    if(mysqli_num_rows($res) > 0)
-             //   {
-                //    while ($row = $res -> fetch_assoc()){
-                    
-                
-                //  $ext= explode('|',$row['image']);
-                 // $path = $ext[1];
-             // }}?>
-             
-             <div class="wrapper"> 
+                       if($conn -> query($sql) == true){?>
+                    echo   '<script>
+                           alert("ingredients added");
+                        </script>';
 
-          <form id="form1" action="Add-ingredients.php" method="post" enctype ="multipart/form-data">
-       
+                     
+                     <?php } else {  ?>
+                      echo '<script>
+                           alert("ingredients not added try again");
+                        </script>';
+
+                    <?php }
+                    }
                   
-   
-          <div class="img-upload">
+                        
+         
+    
+   ?>
 
-          </div>
 
-           <div class="buttons" style="text-align: center;">
-        <ul id="ul-btn" style="list-style:none; display:flex;">
-          <li> <div class= "btn-pp" > <a class="aa-btn" 
-            href="receipe_details_view.php">Receipe Details </a></div></li>
-          <li><div class="btn-pp" style="background-color: white;"><a class="aa-btn" href="#">Ingredients</a></div></li>
-          <li><div class="btn-pp"><a class="aa-btn" href="#">Directions</a></div></li>
+  <div id="page">
+    <!------sideNavBar-------------->
+    <?php include('event_calendar.php'); ?>
+             
+<form action=" " method="post" enctype="multipart/form-data">
+    <div id="content">
+      <!------------------------------------------------DASHBOARD--------------------------------------------------------->
+        <h3 style="font-size: 30px; color: #202224; font-weight: 400; margin: 5px;">New Recipe</h3>
+        <button id="save" name="save" style="height:46px; width:151px; background:#D257E6; color:white;
+         text-align:center; border:none;border-radius:15px;">Save </button> 
+    
 
+        <center>
+            
+        <input type="file" class="custom-file-input"  name="my_image">
+
+       
+           <br><br>
+           </center>
+
+
+<div class="tab">
+  <button class="tablinks" onclick="openCity(event, 'recipe')">Recipe</button>
+  <button class="tablinks" onclick="openCity(event, 'ingredients')">Ingredients</button>
+  <button class="tablinks" onclick="openCity(event, 'directions')">Directions</button>
 </div>
 
-     <div class ="btn-symbol-head">
-        <div class ="symbol" style="position: absolute;box-sizing: border-box;color: purple;
-        height:52px;width:52px;margin-top: 0px; border-radius: 9px; border-color: purple; border-style: solid; text-align: center; cursor: pointer;"><h2> + <h2>
+   <form action=" " method="post" enctype="multipart/file-type">
+<!-----------------recipe tab---------->
+<div id="recipe" class="tabcontent">
+   <?php header('location:create_recipe_new.php'); ?>    // redirect to create recipe page
         </div>
-        <p id="add-ind">Add ingredients</p>
-     </div>
+
+    <!----------ingredient tab------->
+    <div id="ingredients" class="tabcontent">
+         <div class="flex-box">
+           <div class="box">
+              <h2 id="open-ingd" style="color: #BF59EA; font-weight:500; font-size:25px;cursor:pointer; margin-top:15px;">+ </h2>
+        </div>
+        <div class="link">
+          <p id="link-click" style="color:black;"> Add more ingredients </p>
+        </div>
+        </div>
+      
+
+        </div>
 
 
 
-     <!----popup window----->
-     <div class ="popup-ingredients">
-        <div class ="header">
-        <h2 id = "header"> Add Ingredients </h2>
-         </div>
-        <div class="box">
+        <!--------add-ingredients tab ------------->
+        <div id="add-ingrd" class="add-ingrd" style="height:334px; width:456px;border-radius:15px;border:1px solid;">
 
-        <!--------image upload-------->
-          <div class="ing-img" style="height:70px;width:70px;border-radius:16px;position:fixed;border-color:purple; border:solid; text-align:center">
-         <img src="./images/plus.svg" id="sign" style="height:35px; width:35px; cursor:pointer;">
-        <input type ="file" name="my_image" id="my_image" style="height:66px; width:66px;"> 
-         <input type="file" name="my_image" id="my_image" style="display:none;">
-                </div>
+            <div class="flex-box1">
+                   <div class="ingrd-header">
+                      <h5 id="aid"> Add Ingredients  </h5>
+           </div>
 
-
-        <input type= "text" id="text1" class="input-text" name = "text1" placeholder="Name of ingredient" style="top:51px;">
-        <input type="number" id ="text2"  class="input-text" name ="text2" placeholder="Quantity" style="top:88px;">
-        <p id="add-more" name="add-more" style="position:absolute;left:110px;top:70px; color: #7963FB; cursor:pointer;">
-        Add more Ingredients</p>
+        <div class="ingrd-content">
+          <div class="image-add">
+            <h3 id="img-add" style="margin-left:20px; margin-top:15px;">+ </h3>
+            <input type="file" name="my_image" id="my_image" style="display:none;>
+        </div>
+      
+        <div class="left-flex">
+          <input type="text" class="input1" name="ingrd-name" id="ingrd-name" placeholder="Name of ingredient"> <br><br>
+          <input type="text" class="input1" name="quantity" id="quantity" placeholder=" quantity">
+          <p id="pid" style="#BF59EA" style="margin-left:30px;"> Add more ingredients </p>
+          <button type="submit" name="add" id="add"> Done </button>
+        </div>
         
-    <button type="submit"   name="Done" id="Done" style="position: absolute;font-family:'NATS';font-size: 24px;width:162px;border-radius: 10px; background: linear-gradient(267.44deg, rgba(204, 87, 231, 0.66) 0.01%, rgba(116, 99, 252, 0.66) 85.22%);color: white;left:110px;height: 31px; top: 100px;text-align: center;">Done</button>
-            
-            </div>
-            </form>
+                  </div>  
+        </div>
 
+        
+        
+        </form>
+        <!--------direction tab------------>
+        <div id="direction" class="tabcontent">
+        </div>
+
+      
+
+
+                
+        </div>
+        </form>   
+
+        
             <!-------js code for add  more ingredients------>
             <script>
             $(document).ready(function(){
@@ -161,7 +374,27 @@ src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js">
              });
             </script>
 
+    <!--    JS FOR TABS  -->
+<script>
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i <  tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+    event.preventDefault();
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" activee", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " activee";
+  event.preventDefault();
+}
+</script>
           
+
+
 </div>
 </div>
 </div>
