fb35739ded4fd9dc54f586914efcf79b26180164
Janhavi Komurlekar <j.komurlekar@somaiya.edu>
2023-01-08 21:12:07 +0530

    plan changes

---
+++ myplan.php
@@ -0,0 +1,243 @@
+<?php
+// Include config file
+require_once "config.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <style>
+    @import url('https://fonts.googleapis.com/earlyaccess/nats.css');
+
+    body {
+        font-family: 'NATS', serif !important;
+        letter-spacing: 1px;
+    }
+
+    .card {
+        /* background-color: dodgerblue; */
+        color: black;
+        padding: 1rem;
+        height: auto;
+        width: 100%;
+        background: #FFFFFF;
+        box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.25);
+        border-radius: 20px;
+    }
+
+    .cards {
+        max-width: 1200px;
+        margin: 0 auto;
+        display: grid;
+        gap: 1rem;
+        grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+    }
+
+    .card-upper {
+        /* width: 100%; */
+    }
+
+    .card-upper-image {
+        /* background-color: pink; */
+        /* width: 20%; */
+
+        display: inline;
+    }
+
+    .card-upper-details {
+        /* background-color: green; */
+        /* width: 60%; */
+        display: inline;
+        /* padding: 1rem; */
+    }
+
+    .card-upper-options {
+        /* background-color: blue; */
+        /* width: 20%; */
+        display: inline;
+    }
+
+    .card-upper-image img {
+        max-width: 100%;
+        max-height: 100%;
+        display: block;
+
+    }
+
+    .tag-element {
+        background-color: #7282FB;
+        color: white;
+        margin: 5px;
+        padding: 5px;
+        border-radius: 5px;
+    }
+
+    .card-middle {
+        padding: 1rem;
+    }
+
+    .card-below {
+        /* margin-top: 5px; */
+        padding: 1rem;
+        /* background-color: red; */
+    }
+
+    .search-box{
+        border:none;
+
+    }
+
+    .search-box:focus{
+        outline:none;
+    }
+
+    .search-icon{
+        border:none;
+        background:white;
+
+    }
+
+    .search-form{
+        border: 1px solid #E1E1E1;
+        width:fit-content;
+        float:right;
+        padding:2px;
+        border-radius : 5px;
+    }
+    </style>
+    <script>
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+    </script>
+</head>
+
+<body>
+    <?php
+include "navbar.php";
+?>
+
+
+    <div class="row" style="padding:1rem;">
+        <div class="col-6" style="font-weight:bold;font-size:20px;">My Plans</div>
+        <div class="col-6" style="text-align:right">
+            <form class="search-form">
+                <input type="search" placeholder="Search plan" class="search-box">
+                <button type="submit" class="search-icon"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="container">
+                <div class="cards">
+                    <?php
+
+                        
+ $sql = "SELECT * FROM create_plan";
+ if($result = mysqli_query($conn, $sql)){
+     if(mysqli_num_rows($result) > 0){
+             while($row = mysqli_fetch_array($result)){
+               ?>
+
+                    <div class="card" container>
+                        <div class="card-upper row">
+                            <div class="card-upper-image col-3">
+                                <img src="images/fruit_salad.svg" alt="">
+                            </div>
+                            <div class="card-upper-details col-8">
+                                <div class="row">
+                                    <div class="col"
+                                        style="margin-top:5px;margin-bottom:5px; font-size:20px;font-weight:bold;">
+                                        <?php echo $row['name']?></div>
+                                    <div class="w-100"></div>
+                                    <div class="col-5" style="margin-top:5px;margin-bottom:5px; "><span
+                                            style="font-weight:bold">Rs.<?php echo $row['price'] ?></span>/month
+                                    </div>
+                                    <div class="col-7"
+                                        style="margin-top:5px;margin-bottom:15px;font-size:13px;font-weight:bold;display:flex;align-items:center;justify-content:center;">
+                                        <?php $orgDate = $row['start_date']; $newDate = date("d/m/Y", strtotime($orgDate)); echo $newDate ?>
+                                        to
+                                        <?php $orgDate = $row['end_date']; $newDate = date("d/m/Y", strtotime($orgDate)); echo $newDate ?>
+                                    </div>
+                                    <div class="w-100"></div>
+                                    <?php
+                                            $mark=explode(',', $row['tags']);//what will do here
+                                            foreach($mark as $out) {
+                                               echo '<div class="tag-element" style="width:auto;">'.$out.'</div>';
+                                            }
+                                            ?>
+
+                                </div>
+                            </div>
+                            <div class="card-upper-options col-1">
+                                <div class="row">
+                                    <div style="display:inline-block">
+                                        <a href="update_plan.php?id=<?php echo $row['plan_id'] ?>" title="Update Record"
+                                            style="color#7282FB" data-toggle="tooltip"><span
+                                                class="fa fa-pencil"></span></a>
+                                    </div>
+                                    <div style="display:inline-block">
+                                        <a onclick="return confirm('Are you sure?')" href="delete_plan.php?id=<?php echo $row['plan_id'] ?>" title="Delete Record"
+                                            style="color:#7282FB" data-toggle="tooltip"><span
+                                                class="fa fa-trash "></span></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-middle row"><?php echo $row['description']?></div>
+                        <div class="card-below row">
+                            <div class="col">
+                                <div class="row">FEATURES</div>
+                                <div class="row">
+                                    <?php
+                                            $mark=explode(',', $row['features']);//what will do here
+                                            foreach($mark as $out) {
+                                                echo '<div class="col-3">';
+                                                echo '<div class="row" style="display:inline-block; margin-right:5px;"><i style="color:black;" class="fa-regular fa-circle-check"></i></div>';
+                                                echo '<div class="row" style="display:inline-block;">'.$out.'</div>';
+                                                echo '</div>';
+                                            }
+                                            ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <?php
+             }
+
+         // Free result set
+         mysqli_free_result($result);
+     } else{
+         echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
+     }
+ } else{
+     echo "Oops! Something went wrong. Please try again later.";
+ }
+
+ // Close connection
+ mysqli_close($conn);
+                        ?>
+
+                    <div>
+                    </div>
+                </div>
+            </div>
+</body>
+
+</html>
--- update_plan.php
+++ update_plan.php
@@ -27,66 +27,66 @@
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <style>
-    /* Add Tags Popup */
+/* Add Tags Popup */
 .popup {
-  display: none;
+    display: none;
 
 }
 
 .popup.open {
-  display: block;
+    display: block;
 }
 
 .blocker {
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  content: ' ';
-  background: rgba(0, 0, 0, .5);
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    content: ' ';
+    background: rgba(0, 0, 0, .5);
 }
 
 .popup .contents {
 
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  width: auto;
-  height: auto;
-  display: flex-box;
-  align-items: center;
-  justify-content: center;
-  background: #FFF;
-  position: fixed;
-  top: 50vh;
-  left: 50vw;
-  transform: translate(-50%, -50%);
-  padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    width: auto;
+    height: auto;
+    display: flex-box;
+    align-items: center;
+    justify-content: center;
+    background: #FFF;
+    position: fixed;
+    top: 50vh;
+    left: 50vw;
+    transform: translate(-50%, -50%);
+    padding: 20px;
 }
 
 .popup .contents p {
-  text-align: center;
-  margin-top: 5px;
+    text-align: center;
+    margin-top: 5px;
 }
 
 .popup .contents input {
-  text-align: center;
-  margin-top: 5px;
-  /* margin-left:5px;
+    text-align: center;
+    margin-top: 5px;
+    /* margin-left:5px;
   margin-right:5px; */
 }
 
 .popup .contents button {
-  text-align: center;
-  margin-top: 15px;
-  margin-left: 30%;
-  text-decoration: none;
-  border: none;
-  background-color: #6883FB;
-  color: white;
-  padding: 5px;
-  width: 40%;
-  border-radius: 5px;
+    text-align: center;
+    margin-top: 15px;
+    margin-left: 30%;
+    text-decoration: none;
+    border: none;
+    background-color: #6883FB;
+    color: white;
+    padding: 5px;
+    width: 40%;
+    border-radius: 5px;
 
 
 }
@@ -95,100 +95,100 @@
 
 
 .features_main {
-  display: flex;
-  flex-direction: row;
-  /* align-items: center;
+    display: flex;
+    flex-direction: row;
+    /* align-items: center;
   justify-content: center; */
-  margin-top: 20px;
-  width: 100%;
+    margin-top: 20px;
+    width: 100%;
 }
 
 .features_left {
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
 
-  width: 100%;
+    width: 100%;
 }
 
 .features_right {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
 }
 
 .features_box {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  padding: 6px;
-  height: 50px;
-  width: 70%;
-  background: #FFFFFF;
-  box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-  margin: 5px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 6px;
+    height: 50px;
+    width: 70%;
+    background: #FFFFFF;
+    box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    margin: 5px;
 }
 
 .ip_box {
-  background: #FFFFFF;
-  box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);
-  border-radius: 10px;
-  border: none;
-  width: 400px;
-  padding: 10px;
+    background: #FFFFFF;
+    box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    border: none;
+    width: 400px;
+    padding: 10px;
 }
 
 
 .plan_btn_add {
-  width: 40px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 5%;
+    width: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5%;
 }
 
 .plan_btn {
-  color: #FFFFFF;
-  background: #7282FB;
-  border-radius: 10px;
-  height: 40px;
-  padding-top: 15px;
-  padding-bottom: 15px;
-  padding-left: 5px;
-  padding-right: 5px;
-  margin-right: 10px;
-  font-family: 'NATS';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 21px;
-  line-height: 24px;
-  border: none;
-  /* identical to box height, or 104% */
+    color: #FFFFFF;
+    background: #7282FB;
+    border-radius: 10px;
+    height: 40px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    padding-left: 5px;
+    padding-right: 5px;
+    margin-right: 10px;
+    font-family: 'NATS';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 21px;
+    line-height: 24px;
+    border: none;
+    /* identical to box height, or 104% */
 
-  display: flex;
-  align-items: center;
-  text-align: right;
-  letter-spacing: 0.44px;
+    display: flex;
+    align-items: center;
+    text-align: right;
+    letter-spacing: 0.44px;
 }
 
 .input-group input {
-  border: none;
+    border: none;
 }
 
 .input-group input:focus {
-  outline: none !important;
+    outline: none !important;
 }
 
 .remove-textbox {
-  color: black;
-  background: white;
-  border: none;
+    color: black;
+    background: white;
+    border: none;
 }
-
 </style>
+
 <body>
     <!-- Navbar Start -->
     <?php
@@ -202,6 +202,15 @@ include "navbar.php"
         <h4 class="header">Create Plan</h4>
 
         <!-- Other content -->
+        <?php
+        $id = $_GET['id'];
+        // echo $id ;
+        $sql = "SELECT * from `create_plan` where plan_id='$id'";
+        $result = mysqli_query($conn,$sql);
+        while($row = mysqli_fetch_array($result)){
+            echo $row['name'];
+        ?>
+
         <div class="plan_form">
             <!-- Person image -->
             <div class="event-image">
@@ -210,10 +219,10 @@ include "navbar.php"
                 </div>
             </div>
             <br>
-            <form action="create_plan.php" method="post">
+            <form action="#" method="post">
                 <!-- Plan name -->
                 <div style="text-align:center; !important" class="plan_name_box">
-                    <input type="text" placeholder="Plan Name" class="plan_name" name="plan_name" required>
+                    <input required type="text" placeholder="Plan Name" class="plan_name" name="plan_name" value="<?php echo $row['name']?>" >
                 </div>
 
                 <!-- Main form starts -->
@@ -239,8 +248,6 @@ include "navbar.php"
                                     $('#fields_u').multiselect();
                                     $('#fields_u').multiselect('select', ['Keto Diet']);
                                     $('#fields_u').multiselect('select', ['Vegan Diet']);
-
-
                                     $('#fields_u_add').on('click', function() {
                                         $textValue = document.getElementById("txt").value;
                                         $('#fields_u').append('<option value="' + $textValue + '">' +
@@ -277,15 +284,9 @@ include "navbar.php"
                         <br>
                         <label for="" class="subject tag_title">Plan Duration</label>
                         <p style="font-size:15px;">Start Time</p>
-                        <input class="subject subject-text" type="date" name="start_date" style="height: 45px; background: #FFFFFF;
-    box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;
-    border:none;" />
+                        <input required class="subject subject-text" type="date" name="start_date" value="<?php echo $row['start_date']?>" style="height: 45px; background: #FFFFFF;box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);border-radius: 10px;border:none;" />
                         <p style="font-size:15px; margin-top:10px;">End Time</p>
-                        <input class="subject subject-text" type="date" name="end_date" style="height: 45px; background: #FFFFFF;
-    box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);
-    border-radius: 10px;
-    border:none;" />
+                        <input required class="subject subject-text" type="date" name="end_date" value="<?php echo $row['end_date']?>" style="height: 45px; background: #FFFFFF;box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);border-radius: 10px;border:none;" />
                         <br>
 
                         <label for="" class="subject tag_title">Features</label>
@@ -293,7 +294,6 @@ include "navbar.php"
                         <div class="features_main">
                             <div class="features_left textbox-wrapper">
                                 <div class="input-group">
-
                                 </div>
 
                             </div>
@@ -306,43 +306,42 @@ include "navbar.php"
 
                         <br>
                         <label for="" class="subject tag_title">Description</label>
-                        <input class="subject " type="text" name="description" style="height: 79px;  background: #FFFFFF;
+                        <input required class="subject " type="text" name="description" value="<?php echo $row['description']?>" style="height: 79px; background: #FFFFFF;
     box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
     border:none;" />
                         <br />
 
                         <label for="" class="subject tag_title">Price</label>
-                        <input class="subject subject-text" type="number" name="price" placeholder="Rs. /month" style="height: 45px; background: #FFFFFF;
+                        <input required class="subject subject-text" type="number" value="<?php echo $row['price']?>" name="price" placeholder="Rs. /month" style="height: 45px; background: #FFFFFF;
     box-shadow: 0px 1.7px 5px rgba(0, 0, 0, 0.25);
     border-radius: 10px;
     border:none;" />
-
                         <br>
                         <div class="btns">
                             <button>Cancel</button>
-                            <button style="float: right;" name="final_save_btn" type="submit">Save</button>
+                            <button style="float: right;" name="final_save_btn" type="submit">Update</button>
                         </div>
                         <br><br><br>
                     </div>
-
                 </div>
-
             </form>
-
         </div>
-
+        <?php }
+        ?>
     </div>
 
 
     <!-- Contents End -->
+
     <!-- Add everything in database -->
     <?php
 if (isset($_POST['final_save_btn'])){
+        
+        $id = $_GET['id'];
     // For tags
-    $name_arr = $_POST['languages'];
-    $tags = implode(" , ",$name_arr);
-    // $tags = "hello" ;
+        $name_arr = $_POST['languages'];
+        $tags = implode(" , ",$name_arr);
         $planname = $_POST['plan_name'];
         // dieticianid....This is a session variable
         $profile = "Azarudeen";
@@ -353,15 +352,23 @@ if (isset($_POST['final_save_btn'])){
         $price = $_POST['price'];
         $myarray = $_POST['text_arr'];
         $features = implode(" , ",$myarray);
-  
-            $sql1 = "INSERT INTO `create_plan` (`profile`,`name`,`tags`, `start_date`, `end_date`,`features`, `description`, `price`) VALUES ('$profile','$planname','$tags','$start_date','$end_date','$features','$description','$price')";
-            $result1=mysqli_query($conn,$sql1);
+
+
+        $sql1 = "UPDATE `create_plan` SET plan_id=$id,name='$planname',tags='$tags',start_date='$start_date',end_date='$end_date',features='$features',description='$description',price='$price' WHERE plan_id=$id";
+        $result1=mysqli_query($conn,$sql1);
+
+        if($result){
+
+        }
+        else{
+            die(mysqli_error($conn));
+        }
    
 }
             ?>
 
     <!-- Pop up ends -->
-
+'
 </body>
 <script>
 $(document).ready(function() {
@@ -381,7 +388,6 @@ function showPopup() {
 function hidePopup() {
     popup.classList.remove('open');
 }
-
 </script>
 
 
