57ebc94ca0d714d657a98ca8eb3b425c37ccf92c
chetramaaseri <90443950+chetramaaseri@users.noreply.github.com>
2023-03-23 00:32:31 +0530

    default link

--- index.php
+++ index.php
@@ -5,6 +5,15 @@ include('navbar.php');
 if(isset($_SESSION['dietitianuserID'])){
     $dietitian_id = $_SESSION['dietitianuserID'];
 
+    $user=$_SESSION['name'];
+    $sql="SELECT * FROM addclient WHERE dietitianuserID='$user'";
+    $q="SELECT * FROM create_event WHERE dietitianuserID='$user'";
+    $result = $conn->query($sql);
+    $req= $conn->query($q);
+    if(mysqli_num_rows($result)<1 && mysqli_num_rows($req)<1){
+        header('Location:upcomingevents.php');
+    }
+    
     # database connection file
     include 'app/db.conn.php';
 
